37e8b703a766bf7d9949c18935bfa63e18fe2b59
Bailey Conradt <baileyconradt@gmail.com>
2019-07-31 10:51:12 -0500

    added auto-pause to music page

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 ﻿//this is the javascript file
+
+document.addEventListener('play', function(e){
+    var audios = document.getElementsByTagName('audio');
+    for(var i = 0, len = audios.length; i < len;i++){
+        if(audios[i] != e.target){
+            audios[i].pause();
+        }
+    }
+}, true);
