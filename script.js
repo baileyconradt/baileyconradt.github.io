4046c347f78f07e89f163872ce46bce9570a2a91
Bailey Conradt <baileyconradt@gmail.com>
2019-07-31 11:59:32 -0500

    added mobile menu

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 ﻿//this is the javascript file
 
+
+//toggles auto-pause for other songs when playing a new song
 document.addEventListener('play', function(e){
     var audios = document.getElementsByTagName('audio');
     for(var i = 0, len = audios.length; i < len;i++){
