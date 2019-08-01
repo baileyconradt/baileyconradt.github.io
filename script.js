a04fe5aaae9de3df71326372251baf1359f80e16
Bailey Conradt <baileyconradt@gmail.com>
2019-08-01 10:59:02 -0500

    added secret code

--- script.js
+++ script.js
@@ -10,3 +10,33 @@ document.addEventListener('play', function(e){
         }
     }
 }, true);
+
+//hidden function
+function Konami(keyCodes, callback) { 
+
+    const goal = keyCodes.length 
+    let targetIndex = 0 
+    let score = 0 
+
+    const reset = () => { 
+        targetIndex = 0 
+        score = 0 
+    } 
+
+    document.addEventListener('keydown', function (e) { 
+        if (e.keyCode === keyCodes[targetIndex]) { 
+            score += 1 
+            targetIndex += 1 
+            if (score === goal) { 
+                reset() 
+                callback() 
+            } 
+        } else { 
+            reset() 
+        } 
+    }) 
+} 
+
+Konami([69, 65, 83, 84], () => { 
+    window.location = 'https://www.youtube.com/watch?v=BHS0482sc0E&feature=youtu.be' 
+}) 
